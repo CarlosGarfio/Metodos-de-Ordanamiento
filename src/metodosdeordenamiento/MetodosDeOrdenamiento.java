bc49095cc46d38597f21959f7b1f87c93e554a88
gmcar <gmcar@192.168.1.64>
2019-01-30 16:01:58 -0700

    Agregue el metodo Insercion.

--- src/metodosdeordenamiento/MetodosDeOrdenamiento.java
+++ src/metodosdeordenamiento/MetodosDeOrdenamiento.java
@@ -3,7 +3,13 @@ package metodosdeordenamiento;
 public class MetodosDeOrdenamiento {
 
     public static void main(String[] args) {
-        System.out.println("Hola");
+        System.out.println("Metodos de ordenamiento.\n-------------------------");
+        System.out.println("Metodo Insercion(Garfio y Diana): "+Insercion());
+    }
+    
+    public static long Insercion(){
+        System.currentTimeMillis();
+        return 0L;
     }
     
 }
