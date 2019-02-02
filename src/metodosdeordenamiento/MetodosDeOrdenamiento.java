bb2a25eee80eb0d382a4eb798ca0ef334c99fc69
gmcar <gmcar@192.168.1.64>
2019-02-02 16:08:30 -0700

    Listo

--- src/metodosdeordenamiento/MetodosDeOrdenamiento.java
+++ src/metodosdeordenamiento/MetodosDeOrdenamiento.java
@@ -3,19 +3,28 @@ package metodosdeordenamiento;
 import Arreglo.Arreglo;
 
 public class MetodosDeOrdenamiento {
-    
+    static Arreglo arreglo = new Arreglo(3, 3);
+    static long orden;
     public static void main(String[] args) {
-        Arreglo arreglo = new Arreglo(3, 3);
         System.out.println("Metodos de ordenamiento.\n-------------------------");
         System.out.println("Metodo de inserción:");
         System.out.println("Matriz generada:");
-        arreglo.fill(0,11);
-        arreglo.print();
+        arreglo.fill(0,11);//Se llena la matriz
+        arreglo.print();//Se imprime
         System.out.println("Matriz ordenada:");
-        long orden=arreglo.Insercion();
+        orden=arreglo.Insercion();//Se ordena
         arreglo.print();
         System.out.println("Metodo Insercion(Garfio y Diana): "+orden);
         System.out.println("-------------------------");
-        
+        //Formato (Copien y peguen eso)
+        System.out.println("Metodo de :");
+        System.out.println("Matriz generada:");
+        arreglo.fill(0,11);//Se llena la matriz
+        arreglo.print();//Se imprime
+        System.out.println("Matriz ordenada:");
+        orden=arreglo.Insercion();//Se ordena
+        arreglo.print();
+        System.out.println("Metodo Insercion(Integrantes): "+orden);
+        System.out.println("-------------------------");
     }
 }
